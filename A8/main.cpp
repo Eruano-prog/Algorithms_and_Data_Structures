07b26eb2da7b2090ce3ad1f38e4193ab0146fbbb
Eruano <fdgolovlev@gmail.com>
2023-02-17 20:54:31 +0300

    Final

--- A8/main.cpp
+++ A8/main.cpp
@@ -1,101 +1,49 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <queue>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-struct Node{
-    char data;
-    struct Node *next;
-};
-
-struct List{
-    struct Node* Head = nullptr;
-    struct Node* Tail = nullptr;
-
-    void enqueue(char data){
-        struct Node* node = (struct Node*) malloc(sizeof(struct Node*));
-        node->data = data;
-        node->next = nullptr;
-
-        if(Head == nullptr){
-            Head = node;
-            Tail = node;
-        }
-        else{
-            Tail->next = node;
-            Tail = node;
-        }
+void next_letter(std::string const &string, int &iter, int &num, char &letter){
+    letter = string[iter];
+    iter++;
+    while(iter < string.length() & string[iter] < 64){
+        num = num * 10 + string[iter] - 48;
+        iter++;
     }
-    char dequeue(){
-        struct Node *temp = Head;
-        Head = Head->next;
-        char n = temp->data;
-        free(temp);
-        return n;
-    }
-};
-
+}
 
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
+    std::string sentM, sentS;
+    cin >> sentM >> sentS;
 
-    char ch, letter;
-    int count = 0;
-    std::vector<char> SentM;
-    int number = 0;
-    cin >> letter;
+    int num1=0, num2=0, minim, diffs;
+    char let1, let2;
+    int it1=0, it2=0;
+    while(it1 < n || it2 < m){
+        if(num1 == 0) next_letter(sentM, it1, num1, let1);
+        if(num2 == 0) next_letter(sentS, it2, num2, let2);
 
-    for (int i = 0; i < n-1; ++i) {
-        cin >> ch;
-        if (ch > 64){
-            for (int j = 0; j < number; ++j) {
-                SentM.push_back(letter);
-            }
-            letter = ch;
-            count += number;
-            number = 0;
+        minim = std::min(num1, num2);
+
+        if(let1 != let2){
+            diffs += minim;
         }
-        else{
-            number = number * 10 + ch - 48;
-        }
-    }
-    count += number;
-    for (int j = 0; j < number; ++j) {
-        SentM.push_back(letter);
+        num1 -= minim;
+        num2 -= minim;
+
     }
 
-
-    std::vector<char> SentS;
-    number = 0;
-    cin >> letter;
-    int ptr = 0;
-    int diffs = 0;
-
-    for (int i = 0; i < m - 1; ++i) {
-        cin >> ch;
-        if (ch > 64){
-            for (int j = 0; j < number; ++j) {
-                if (SentM[ptr] != letter) {
-                    diffs++;
-                    ptr++;
-                }
-            }
-            letter = ch;
-            number = 0;
-        }
-        else{
-            number = number * 10 + ch - 48;
-        }
-    }
-
-    if (diffs < k){
-        cout << "Yes";
-    }
-    else{
+    if (diffs > k){
         cout << "No";
     }
+    else{
+        cout << "Yes";
+    }
+
     return 0;
 }

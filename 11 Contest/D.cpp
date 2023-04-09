c1e0c3b0e3a04aa1c7d6624123467eed1db08baf
Eruano <fdgolovlev@gmail.com>
2023-04-10 00:22:33 +0300

    D done

--- 11 Contest/D.cpp
+++ 11 Contest/D.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <queue>
 
-using std::cout;
 using std::cin;
+using std::cout;
+using std::string;
+using std::swap;
 
 struct Path{
     int node, weight;
@@ -13,45 +15,107 @@ struct Path{
 
 struct Node{
     std::vector<Path> paths;
-    int dist = -1;
+    int dist = 10000000;
+    bool visited = false;
 };
 
-struct comp{
-    bool operator()(Node* a, Node* b){
-        return a->dist < b->dist;
+class prior_queue {
+public:
+    std::vector <Node*> heap;
+
+    void siftDown() {
+        int i = 0;
+        while (2 * i + 1 < heap.size()) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int j = left;
+            if (right < heap.size() && heap[right]->dist < heap[left]->dist) {
+                j = right;
+            }
+            if (heap[i]->dist <= heap[j]->dist) {
+                break;
+            }
+            swap(heap[i], heap[j]);
+            i = j;
+        }
+    }
+
+    void siftUp(int pos) {
+        while ((pos - 1) / 2 >= 0 and heap[pos]->dist < heap[(pos - 1) / 2]->dist) {
+            swap(heap[pos], heap[(pos - 1) / 2]);
+            pos = (pos - 1) / 2;
+        }
+    }
+
+
+    Node* extract() {
+        Node* temp = heap[0];
+        swap(heap[0], heap[heap.size() - 1]);
+        heap.pop_back();
+        if (heap.size() > 1) { siftDown(); }
+        return temp;
+    }
+
+    void push(Node* node){
+        heap.push_back(node);
+        siftUp(heap.size() - 1);
+    }
+
+    void decrease(Node* position) {
+        for (int i = 0; i < heap.size(); i++) {
+            if (heap[i] == position) {
+                siftUp(i);
+                return;
+            }
+        }
+    }
+
+    bool empty(){
+        return heap.size() == 0;
     }
 };
 
 class Graph{
     std::vector<Node*> nodes;
+    int count = 0;
 
 public:
     Graph(int t){
         nodes.reserve(t);
         for (int i = 0; i < t; ++i) {
-            nodes[i] = new Node;
+            nodes.push_back(new Node);
         }
     }
 
     void add_path(int b, int e, int w){
         nodes[b]->paths.emplace_back(e, w);
+        nodes[e]->paths.emplace_back(b, w);
     }
 
     void Dijkstra(int s){
-        std::priority_queue<Node*, std::vector<Node*>, comp> pq;
+        prior_queue pq;
         nodes[s]->dist = 0;
-        Node* cur = nodes[s];
+        Node* cur;
+        pq.push(nodes[s]);
         bool changed;
         while(!pq.empty()){
+            cur = pq.extract();
             for (auto path:cur->paths) {
                 changed = Relax(cur, path);
                 if (changed){
                     pq.push(nodes[path.node]);
                 }
             }
-            cur = pq.top();
-            pq.pop();
+            count++;
+            cur->visited = true;
         }
+    }
+
+    int get_dist(int Q){
+        if (nodes[Q]->dist == 10000000){
+            return -1;
+        }
+        return nodes[Q]->dist;
     }
 
 private:
@@ -67,13 +131,28 @@ private:
 int main(){
     int N, M, Q, S;
     cin >> N >> M >> Q >> S;
-
+    S--;
     Graph graph(N);
 
     int B, E, W;
     for (int i = 0; i < M; ++i) {
         cin >> B >> E >> W;
+        B--; E--;
+        graph.add_path(B, E, W);
     }
 
+    graph.Dijkstra(S);
 
+    int p;
+    std::queue<int> ans;
+    for (int i = 0; i < Q; ++i) {
+        cin >> p;
+        p--;
+        ans.push(graph.get_dist(p));
+    }
+
+    while(!ans.empty()){
+        cout << ans.front() << std::endl;
+        ans.pop();
+    }
 }

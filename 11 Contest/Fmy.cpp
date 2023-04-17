a3283ca22ba898a042f9ab2f4e46dafe7b80b245
Eruano <fdgolovlev@gmail.com>
2023-04-18 00:57:17 +0300

    F done

--- 11 Contest/Fmy.cpp
+++ 11 Contest/Fmy.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using std::vector;
 using std::pair;
@@ -11,11 +12,6 @@ vector<pair<long long*, long long>> history;
 class DSU{
 public:
     vector <long long> parent, rang;
-
-    DSU(int n){
-        parent.resize(n, -1);
-        rang.resize(n, 0);
-    }
 
     long long root(long long v){
         while (parent[v] != -1){
@@ -54,22 +50,90 @@ struct order{
 vector<order> orders;
 
 class Segment_Tree{
-    vector<pair<long long, long long>> tree;
+public:
+    vector<vector<pair<long long, long long>>> tree;
+    DSU dsu;
 
-    Segment_Tree(int n){
-        tree[n*4];
+    Segment_Tree(int N){
+        dsu.parent.resize(N, -1);
+        dsu.rang.resize(N, 0);
+        orders.resize(N);
+        tree.resize(N*4);
     }
 
     void add(long long left, long long right, pair<long long, long long> edge, long long cur, long long cur_left, int cur_right){
         if (left > cur_right || right < cur_left) return;
 
         if (left <= cur_left && right >= cur_right) {
-            tree[cur].
+            tree[cur].push_back(edge);
+            return;
         }
 
+        long long mid = (cur_right+cur_left) / 2;
+        add(left, right, edge, cur*2+1, cur_left, mid);
+        add(left, right, edge, cur*2+2, mid+1, cur_right);
+    }
+
+    void traversal(long long cur, long long cur_left, long long cur_right){
+        long long start = history.size();
+
+        for (auto edge : tree[cur]) {
+            dsu.merge(edge.first, edge.second);
+        }
+
+        if (cur_left == cur_right){
+            if (orders[cur_right].com == '?'){
+                if(dsu.root(orders[cur_right].v) == dsu.root(orders[cur_right].u)){
+                    cout << "YES" << std::endl;
+                }
+                else{
+                    cout << "NO" << std::endl;
+                }
+            }
+        }
+        else{
+            long long mid = (cur_left + cur_right) / 2;
+            traversal(cur * 2 + 1, cur_left, mid);
+            traversal(cur * 2 + 2, mid+1, cur_right);
+        }
+
+        while ((long long)history.size() > start){
+            *history.back().first = history.back().second;
+            history.pop_back();
+        }
     }
 };
 
 int main(){
+    std::ios::sync_with_stdio(false);
+    int n, m, q;
+    cin >> n >> m >> q;
+    Segment_Tree tree((m+q) * 4);
 
+    std::set < pair <pair<long long, long long>, long long> > edges;
+    for (int i = 0; i < m; ++i) {
+        cin >> orders[i].u >> orders[i].v;
+        if (orders[i].u > orders[i].v) std::swap(orders[i].u, orders[i].v);
+        orders[i].com = '+';
+        edges.emplace(pair<long long, long long>(orders[i].u, orders[i].v), i);
+    }
+
+    for (int i = m; i < q+m; ++i) {
+        cin >> orders[i].com >> orders[i].u >> orders[i].v;
+        if (orders[i].u > orders[i].v) std::swap(orders[i].u, orders[i].v);
+
+        if (orders[i].com == '-'){
+            auto iter = edges.lower_bound(make_pair(pair<long long, long long>(orders[i].u, orders[i].v), 0));
+            tree.add(iter->second, i, iter->first, 0, 0, m + q - 1);
+            edges.erase(iter);
+        }
+    }
+
+    for (auto edge : edges) {
+        tree.add(edge.second, m + q - 1, edge.first, 0, 0, m + q - 1);
+    }
+
+    tree.traversal(0, 0, m + q - 1);
+
+    return 0;
 }

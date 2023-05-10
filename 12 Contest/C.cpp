61c6154b308b7b67633c5391e78c3f67c5601104
Eruano <fdgolovlev@gmail.com>
2023-05-10 18:41:29 +0300

    C tries

--- 12 Contest/C.cpp
+++ 12 Contest/C.cpp
@@ -1,138 +1,102 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <algorithm>
 
-using std::vector;
-using std::cout;
-using std::cin;
+using namespace std;
 
-class DSU{
-public:
-    vector<int> parents;
-    vector<int> rang;
-
-    DSU(int N){
-        N++;
-        parents.resize(N, -1);
-        rang.resize(N, 0);
-    }
-
-    void merge(int a, int b){
-        int root_a = root(a);
-        int root_b = root(b);
-
-        if (rang[root_a] > rang[root_b]){
-            parents[root_b] = root_a;
-        }
-        else if(rang[root_a] < rang[root_b]){
-            parents[root_a] = root_b;
-        }
-        else{
-            parents[root_a] = root_b;
-            rang[root_b]++;
-        }
-    }
-
-    bool check(int a, int b){
-        return root(a) == root(b);
-    }
-
-    int root(int a){
-        int cur = a;
-        while(parents[cur] != -1){
-            cur = parents[cur];
-        }
-        int it = 0;
-        while(parents[a] != -1){
-            it = parents[a];
-            parents[a] = cur;
-            a = it;
-        }
-        return cur;
-    }
-};
+const int MAXN = 2e5 + 5;
 
 struct Edge {
-    int from, to;
-    unsigned long long weight, answer = -1;
-
-    Edge() = default;
-    void fill(int f, int t, long long w){
-        from = f;
-        to = t;
-        weight = w;
-    }
+    int u, v, w, id;
+    Edge(int u, int v, int w, int id) : u(u), v(v), w(w), id(id) {}
 };
 
+vector<Edge> edges;
+int parent[MAXN], rank[MAXN];
+long long mst_weight[MAXN], dist[MAXN];
+bool used[MAXN];
 
-
-void swap(Edge* pF, Edge* pS){
-    Edge key = *pF;
-    *pF = *pS;
-    *pS = key;
-}
-
-
-
-int QuickSort(vector<Edge>& edges, int left, int right){
-    int l = left, r = right;
-    Edge p = edges[(left + right) / 2];
-
-    while(l <= r){
-        while (edges[l].weight < p.weight){
-            l++;
-        }
-        while (edges[r].weight > p.weight){
-            r--;
-        }
-        if(l >= r){
-            break;
-        }
-        swap(&edges[l++], &edges[r--]);
+int find(int x) {
+    if (x == parent[x]) {
+        return x;
     }
-    return r;
+    return parent[x] = find(parent[x]);
 }
 
-void quick(vector<Edge>& edges, int left, int right){
-    if (left < right){
-        int q = QuickSort(edges, left, right);
-        quick(edges, left, q);
-        quick(edges, q+1, right);
+void unite(int x, int y, int w) {
+    x = find(x);
+    y = find(y);
+    if (x == y) {
+        return;
+    }
+    if (rank[x] < rank[y]) {
+        swap(x, y);
+    }
+    parent[y] = x;
+    mst_weight[x] += mst_weight[y] + w;
+    if (rank[x] == rank[y]) {
+        rank[x]++;
     }
 }
 
-
-int main(){
-    int N, M;
-    cin >> N >> M;
-
-    vector<Edge> edges;
-    edges.resize(M);
-    int f, t, w;
-    for (int i = 0; i < M; ++i) {
-        cin >> f >> t >> w;
-        edges[i].fill(f, t, w);
+void kruskal(int n) {
+    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+        return a.w < b.w;
+    });
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+        mst_weight[i] = 0;
     }
-
-    vector<Edge> sorted = edges;
-
-    quick(sorted, 0, M-1);
-    std::stack<Edge*> used;
-    DSU dsu(N);
-    int count = 0;
-    unsigned long long ans = 0;
-    for (int i = 0; i < M; ++i) {
-        auto edge = sorted[i];
-        if (count < N - 1 and !dsu.check(edge.from, edge.to)) {
-            ans += edge.weight;
-            dsu.merge(edge.from, edge.to);
-            used.push(&edge);
-            count++;
+    for (const auto& edge : edges) {
+        int u = edge.u, v = edge.v, w = edge.w, id = edge.id;
+        if (find(u) != find(v)) {
+            used[id] = true;
+            unite(u, v, w);
         }
     }
-    while(!used.empty()){
-        used.top()->answer = ans;
-        used.pop();
+}
+
+void dfs(int u, int p, long long w, vector<vector<pair<int, int>>>& adj) {
+    dist[u] = w;
+    for (const auto& edge : adj[u]) {
+        int v = edge.first, d = edge.second;
+        if (v != p) {
+            dfs(v, u, w + d, adj);
+        }
     }
-    cout << ans << std::endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, int>>> adj(n + 1);
+    for (int i = 1; i <= m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.emplace_back(u, v, w, i);
+    }
+
+    kruskal(n);
+
+    for (const auto& edge : edges) {
+        if (!used[edge.id]) {
+            int v = edge.v, u = edge.u, w = edge.w;
+            adj[u].emplace_back(v, w);
+            adj[v].emplace_back(u, w);
+        }
+    }
+
+    dfs(1, 0, 0, adj);
+
+    long long ans = 0;
+    for (int i = 2; i <= n; i++) {
+        ans += dist[i];
+    }
+    cout << ans << '\n';
+
+    return 0;
 }

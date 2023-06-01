04085be86edf171b391df822bd967dd0303e234a
Eruano <fdgolovlev@gmail.com>
2023-06-02 00:36:15 +0300

    D Done

--- 14 Contest/D.cpp
+++ 14 Contest/D.cpp
@@ -1,34 +1,89 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include <string>
+#include <queue>
 
-int main() {
-    int N;
-    std::cin >> N;
+using std::string;
+using std::cout;
+using std::cin;
 
-    std::map<std::string, int> bookCounts;
-    std::vector<std::string> authors;
+struct Node{
+    string data;
+    int weight=1;
 
-    for (int i = 0; i < N; ++i) {
-        int type;
-        std::cin >> type;
+    Node *l = nullptr, *r = nullptr, *p = nullptr;
+};
 
-        if (type == 1) {
-            std::string author;
-            std::cin >> author;
-            bookCounts[author]++;
-            authors.push_back(author);
-        } else if (type == 2) {
-            int k;
-            std::cin >> k;
-            std::sort(authors.begin(), authors.end());
+class Tree{
+public:
+    Node* Head;
 
-            if (k >= 1 && k <= authors.size()) {
-                std::cout << authors[k - 1] << std::endl;
+    void Insert(const string& key){
+        Node* cur = Head;
+        if (cur == nullptr){
+            Head = new Node;
+            Head->data = key;
+            return;
+        }
+        while (cur != nullptr){
+            if (key <= cur->data){
+                cur->weight++;
+                if (cur->l == nullptr) break;
+                else cur = cur->l;
             }
+            if (key > cur->data){
+                cur->weight++;
+                if (cur->r == nullptr) break;
+                else cur = cur->r;
+            }
+        }
+        if (key < cur->data){
+            cur->l = new Node;
+            cur->l->data = key;
+            cur->l->p = cur;
+        }
+        if (key > cur->data){
+            cur->r = new Node;
+            cur->r->data = key;
+            cur->r->p = cur;
         }
     }
 
-    return 0;
+    string get(int index){
+        Node* cur_node = Head;
+        int cur_indx = 0;
+        int weight_l = cur_node->l == nullptr ? 0 : cur_node->l->weight;
+        while(cur_indx + weight_l + 1 != index){
+            if (cur_indx + weight_l + 1 < index){
+                cur_indx = cur_indx + weight_l + 1;
+                cur_node = cur_node->r;
+            }
+            else if (cur_indx + weight_l + 1 > index){
+                cur_node = cur_node->l;
+            }
+            weight_l = cur_node->l == nullptr ? 0 : cur_node->l->weight;
+        }
+        return cur_node->data;
+    }
+};
+
+int main(){
+    int N;
+    cin >> N;
+    string str;
+    Tree tree{};
+
+    for (int i = 0; i < N; ++i) {
+        cin >> str;
+
+        if (str[0] >= '0' and str[0] <= '9'){
+            int sum = 0;
+            for (auto ch : str) {
+                sum = (sum*10) + ch-48;
+            }
+            cout << tree.get(sum) << '\n';
+        }
+        else{
+            tree.Insert(str);
+        }
+    }
 }
--- 14 Contest/E.cpp
+++ 14 Contest/E.cpp
@@ -2,71 +2,103 @@
 #include <string>
 #include <regex>
 
+using std::regex_match;
 using std::string;
 using std::regex;
 using std::cout;
 using std::cin;
 
-bool isValidFullName(const string& fullName) {
+bool isValidName(const string& fullName) {
     // Формат ФИО: Первая буква каждого слова должна быть заглавной, только латинские буквы
-    regex regex("^[A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+ $");
-    return regex_match(fullName, regex);
+    regex reg("^[A-Z][a-z]+?");
+    return regex_match(fullName, reg);
 }
 
 bool isValidAge(const string& age) {
     // Формат возраста: 2 цифры
-    regex regex("^[0-9]{2}$");
-    return regex_match(age, regex);
+    regex reg("[0-9]{2}");
+    return regex_match(age, reg);
 }
 
-bool isValidPhoneNumber(const std::string& phoneNumber) {
+bool isValidPhoneNumber(const string& phoneNumber) {
     // Формат номера телефона:
     // +7(967)645-7442
     // +79676567442
     // 79676557542
     // 89686557442
     // +7(967)-666-74-42
-    regex regex1(R"(^\+?\d{1,2}\(?\d{3}\)?[-\d]{7,10}$)");
-//    regex regex2(R"(^\+?\d{1,2}\(?\d{3}\)?[-\d]{7,10}$)");
-    return std::regex_match(phoneNumber, regex1);
+    regex regex1(R"(\+?7\d{10})");
+    regex regex2(R"(8\d{10})");
+    regex regex4(R"(\+7\(\d{3}\)-?\d{3}-?\d{2}-?\d{2})");
+    return regex_match(phoneNumber, regex1) or regex_match(phoneNumber, regex2) or regex_match(phoneNumber, regex4);
 }
 
 bool isValidAddress(const string& address) {
     // Формат адреса: g.XXX ul.XXX d.XXXXXX
-    regex reg(R"(^g\.[A-Z]{3}, ul\.[A-Z][a-z]+, d\.[0-9][0-9]+ $)");
+    regex reg(R"(g\.[A-Z]{3}, ul\.[A-Z][a-z]+, d\.[0-9]\d+$)");
     return regex_match(address, reg);
 }
 
 int main() {
     int N;
     cin >> N;
+    cin.ignore();
 
     string str;
+    bool flag = true;
     for (int i = 0; i < N; ++i) {
-        string full_name, age, phone, address;
-        for (int j = 0; j < 3; ++j) {
-            cin >> str;
-            full_name += str + ' ';
+        string name, age, phone, address;
+
+        std::getline(cin, str);
+
+        int j = 0;
+        while (str[j] != ' ' and j < str.size()){
+            name += str[j];
+            j++;
         }
-
-        cin >> age;
-
-        cin >> phone;
-
-        for (int j = 0; j < 3; ++j) {
-            cin >> str;
-            address += str + ' ';
+        j++;
+        flag = flag and isValidName(name);
+        name = "";
+        while (str[j] != ' ' and j < str.size()){
+            name += str[j];
+            j++;
         }
+        j++;
+        flag = flag and isValidName(name);
+        name = "";
+        while (str[j] != ' ' and j < str.size()){
+            name += str[j];
+            j++;
+        }
+        j++;
+        flag = flag and isValidName(name);
+        name = "";
 
-        cout << '\n' << isValidFullName(full_name);
-        cout << '\n' << isValidAge(age);
-        cout << '\n' << isValidAddress(address);
-//        if (isValidFullName(full_name) and isValidAge(age) and isValidAddress(address)){
-//            cout << "YES\n";
-//        }
-//        else{
-//            cout << "NO\n";
-//        }
+        while (str[j] != ' ' and j < str.size()){
+            age += str[j];
+            j++;
+        }
+        j++;
+        flag = flag and isValidAge(age);
+
+        while (str[j] != ' ' and j < str.size()){
+            phone += str[j];
+            j++;
+        }
+        j++;
+        flag = flag and isValidPhoneNumber(phone);
+
+        for (;j < str.size(); j++) {
+            address += str[j];
+        }
+        flag = flag and isValidAddress(address);
+
+        if (flag){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
     }
 
     return 0;
